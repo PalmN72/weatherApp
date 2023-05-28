5f9f38debff6eb0b58b45ba112faa284452092ab
Sebastian Palmqvist <sebastian.palmqvist@appliedtechnology.se>
2023-05-28 11:55:43 +0200

    feat: added dateFunc

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,37 @@
 import "./styles/index.css";
 
 const url = 'https://api.open-meteo.com/v1/metno?latitude=59.64&longitude=17.08&hourly=temperature_2m&current_weather=true'
-
 const berga = document.querySelector('.berga')
 const bergaBtn = document.querySelector('#bergaBtn')
 
-const callWeather = async () => await (await fetch(url)).json()
+const callWeatherApi = async () => await (await fetch(url)).json()
 
-const enkp = async () => {
-const data = await callWeather()
-const data2 = data.current_weather
-const temp = data2.temperature
-const windspeed = data2.windspeed
-const windDirection = data2.winddirection
+const structureData = async () => {
+  const data = await callWeatherApi()
 
-return [temp, windspeed, windDirection]
+  return [data.current_weather.temperature, data.current_weather.windspeed, data.current_weather.winddirection]
 }
 
-const weatherDiv = async () => {
+const dateFunc = () => {
+  const date = new Date();
+  const todaysDate = date.toLocaleDateString();
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  return [todaysDate, hours, minutes];
+};
+
+const appendToDom = async () => {
   const divi = document.createElement('div')
-  const [temp, windspeed, windDirection] = await enkp()
-  divi.innerHTML = `<p>The temp in Enköping is ${temp}C. The windspeed is ${windspeed}m/s at ${windDirection} degrees.</p>`
+  divi.setAttribute('id', 'dataEnkp')
+  const [date, hour, min] = dateFunc()
+  const [temp, windspeed, windDirection] = await structureData()
+  divi.innerHTML = `<p>${date} ${hour}:${min}</p><p>The temp in Enköping is ${temp}C. The windspeed is ${windspeed}m/s at ${windDirection} degrees.</p>`
   berga.append(divi)
 }
 
-bergaBtn.addEventListener('click', weatherDiv)
+bergaBtn.addEventListener('click', () => {
+  if (document.querySelector('#dataEnkp')) {
+    document.querySelector('#dataEnkp').remove()
+  }
+  appendToDom()
+})
